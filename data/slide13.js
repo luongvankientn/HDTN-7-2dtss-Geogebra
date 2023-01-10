da7a7d1fb3ed80f933726dc87a29a60fafcd0518
luongvankientn <92704922+luongvankientn@users.noreply.github.com>
2023-01-10 15:40:11 +0700

    Add files via upload

---
+++ data/slide13.js
@@ -0,0 +1,9 @@
+(function(){var loadHandler=window['sl_{E7A63171-A421-4508-A89B-B608BE89962C}'];loadHandler&&loadHandler(12, '<div id="spr0_7ac799af"><div id="spr1_7ac799af" class="kern slide"><img id="img1_7ac799af" src="data/img3.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_7ac799af" class="kern slide"><div id="spr3_7ac799af" style="left:71.852px;top:8px;"><div style="width:0px;"><span id="txt0_7ac799af" data-width="567.101563" style="left:268.873px;top:8.532px;">HOẠT ĐỘNG NHÓM</span></div></div><div id="spr4_7ac799af" style="left:66px;top:118.5px;"><img id="img0_7ac799af" src="data/img17.png" width="668.25" height="483" alt="Vẽ hình trên phần mềm Geogebra\
++ Vẽ ba điểm A, B, C.\
++ Vẽ đường thẳng a đi qua hai điểm A, B.\
++ Vẽ đường thẳng b đi qua điểm C và song song với đường thẳng a.\
++ Vẽ điểm D trên đường thẳng b.\
++ Vẽ đường thẳng c cắt a tại A và cắt b tại C.\
++ Đo và so sánh số đo của hai góc so le trong  𝐷𝐶𝐴  và  𝐵𝐴𝐶 \
++ Đo và so sánh các cặp góc đồng vị\
+"/></div><div id="vd0_7ac799af" style="left:880.062px;top:208px;"></div></div></div>', '{"s":[]}');})();
